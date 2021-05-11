127e145c2796b8b10e8e21419691c5aa8e5ad6ee
juniorh28 <juniorh2897@gmail.com>
2021-05-11 04:21:44 -0400

    fix a typo in player name textbox

--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -76,7 +76,7 @@ public class ScoreKeeper : MonoBehaviour
 
     public void DisplayName()
     {
-        nameTxt.text = "Player:, " + PersistentData.Instance.GetName();
+        nameTxt.text = "Player: " + PersistentData.Instance.GetName();
     }
 }
 
